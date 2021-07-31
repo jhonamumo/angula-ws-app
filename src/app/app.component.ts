7a863e6a2f111839459b015fbf00cfe03e2f8b75
jhonamumo <jhonamumo@gmail.com>
2021-07-30 21:21:28 -0500

    Primer commit del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angula-ws-app';
+  public title: String = 'angula-ws-app';
+  public nombre: String = 'Jhonatan'
+  public showData: boolean = true;
+  //public nombreButton: String = "ocultame";
+  public movies = [
+    {
+      id: 1,
+      title: 'Titanic',
+      description: 'Un barco se hunde en la mitad del pacifico',
+      image: 'https://upload.wikimedia.org/wikipedia/en/1/19/Titanic_%28Official_Film_Poster%29.png'
+    },
+    {
+      id: 2,
+      title: 'Cruella',
+      description: 'Cruella la villana de los dalmatas',
+      image: 'https://es.web.img3.acsta.net/pictures/21/04/21/11/08/5393301.jpg'
+    },
+    {
+      id: 3,
+      title: 'Rápido y furiosos',
+      description: 'Carros y aventura es lo ofrece esta gran pelicula',
+      image: 'https://procinal.com/uploads/PELICULAS/Img_movies/Img_360x500/FF9nuevoposter.jpg'
+    }
+  ];
+
+  public mostrarOcultarDatos(): void {
+    this.showData = !this.showData;
+    //this.nombreButton = this.showData? "ocultame" : "muestrame";
+  }
 }
